e2047884a63fcc5ae6a9f1db923d78b25c393936
The_Fireplace <the.f1repl4ce@gmail.com>
2016-06-20 19:37:12 -0500

    Update scripts

--- setupforge.bat
+++ setupforge.bat
@@ -1,7 +1,6 @@
 @ECHO OFF
-TITLE The_Fireplace's Forge Tools - Setup v1.5
-call gradlew setupDecompWorkspace
-call gradlew idea
+TITLE The_Fireplace's Forge Tools - Setup v1.6
+call gradlew setupDecompWorkspace idea
 ECHO ****************************
 ECHO Forge idea workspace setup complete!
 ECHO ****************************
--- setupforge.sh
+++ setupforge.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 GRADLE_PATH="./gradlew"
-echo "The_Fireplace's Forge Tools - Setup v1.5"
+echo "The_Fireplace's Forge Tools - Setup v1.6"
 
 which gradle && GRADLE_PATH="$(which gradle)"
 
-"$GRADLE_PATH" setupDecompWorkspace
-"$GRADLE_PATH" idea
+"$GRADLE_PATH" setupDecompWorkspace idea
 echo "****************************"
 echo "Forge idea workspaces setup complete!"
 echo "****************************"
