da65422125f599701014cb04fc98991fd4e08824
The_Fireplace <the.f1repl4ce@gmail.com>
2016-06-30 18:09:59 -0500

    1.10.2 update

--- offlinebuild.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-TITLE The_Fireplace's Forge Tools - Offline Build v1.1
-call gradlew --offline build jar
-ECHO ****************************
-ECHO Building mod completed!
-ECHO ****************************
-PAUSE
--- src/main/java/the_fireplace/ias/IAS.java
+++ src/main/java/the_fireplace/ias/IAS.java
@@ -18,7 +18,7 @@ import the_fireplace.iasencrypt.Standards;
 /**
  * @author The_Fireplace
  */
-@Mod(modid=Reference.MODID, name=Reference.MODNAME, clientSideOnly=true, guiFactory="the_fireplace.ias.config.IASGuiFactory", updateJSON = "http://caterpillar.bitnamiapp.com/jsons/ias.json", acceptedMinecraftVersions = "[1.9.4,1.10)")
+@Mod(modid=Reference.MODID, name=Reference.MODNAME, clientSideOnly=true, guiFactory="the_fireplace.ias.config.IASGuiFactory", updateJSON = "http://caterpillar.bitnamiapp.com/jsons/ias.json", acceptedMinecraftVersions = "[1.9.4,1.10.2]")
 public class IAS {
 	public static Configuration config;
 	private static Property CASESENSITIVE_PROPERTY;
