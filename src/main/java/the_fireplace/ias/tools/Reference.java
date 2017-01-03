593ded3f941432c45448682e7381f26334157e9b
The_Fireplace <the.f1repl4ce@gmail.com>
2017-01-03 13:56:39 -0600

    Updated to 1.11.X

--- src/main/java/the_fireplace/ias/tools/Reference.java
+++ src/main/java/the_fireplace/ias/tools/Reference.java
@@ -4,6 +4,6 @@ package the_fireplace.ias.tools;
  * @author The_Fireplace
  */
 public class Reference {
-	public static final String MODID = "IngameAccountSwitcher";
+	public static final String MODID = "ias";
 	public static final String MODNAME = "In-Game Account Switcher";
 }
