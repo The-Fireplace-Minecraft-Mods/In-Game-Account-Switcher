13485ffb9aa461e7d902e602a0faa560f57cd432
VidTuGit <60929459+VidTuGit@users.noreply.github.com>
2021-08-25 02:55:49 +0300

    Update to 7.1.1.

--- src/main/java/ru/vidtu/iasfork/mixins/TitleScreenMixin.java
+++ src/main/java/ru/vidtu/iasfork/mixins/TitleScreenMixin.java
@@ -1,5 +1,6 @@
 package ru.vidtu.iasfork.mixins;
 
+import org.apache.commons.lang3.StringUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -7,15 +8,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.github.mrebhan.ingameaccountswitcher.tools.Config;
 
-import jdk.jshell.JShell;
-import jdk.jshell.Snippet.Status;
-import jdk.jshell.SnippetEvent;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import ru.vidtu.iasfork.Expression;
 import ru.vidtu.iasfork.IASMMPos;
 import the_fireplace.ias.config.ConfigValues;
 import the_fireplace.ias.gui.GuiAccountSelector;
@@ -37,28 +36,19 @@ public class TitleScreenMixin extends Screen {
 			modMenu = FabricLoader.getInstance().isModLoaded("modmenu");
 			skinsLoaded = true;
 		}
-		textX = textY = Integer.MIN_VALUE;
-		new Thread(() -> { //VERY, VERY HACKY CODE, but no external dependencies.
-			if (ConfigValues.TEXT_X.isEmpty() && ConfigValues.TEXT_Y.isEmpty()) {
-				textX = width / 2;
-				textY = height / 4 + 48 + 72 + 12 + (modMenu?32:22);
-				return;
-			}
-			try (JShell js = JShell.create()) {
-	            SnippetEvent s1 = js.eval(ConfigValues.TEXT_X.replace("%width%", Integer.toString(width)).replace("%height%", Integer.toString(height))).stream().filter(c -> c.status() == Status.VALID).findAny().orElse(null);
-	            SnippetEvent s2 = js.eval(ConfigValues.TEXT_Y.replace("%width%", Integer.toString(width)).replace("%height%", Integer.toString(height))).stream().filter(c -> c.status() == Status.VALID).findAny().orElse(null);
-	            if (s1 != null && s2 != null) {
-	            	textX = Integer.parseInt(s1.value());
-	            	textY = Integer.parseInt(s2.value());
-	            } else {
-	            	textX = width / 2;
-	    			textY = height / 4 + 48 + 72 + 12 + (modMenu?32:22);
-	            }
-			} catch (Throwable t) {
+		try {
+			if (StringUtils.isNotBlank(ConfigValues.TEXT_X) && StringUtils.isNotBlank(ConfigValues.TEXT_Y)) {
+				textX = (int) new Expression(ConfigValues.TEXT_X.replace("%width%", Integer.toString(width)).replace("%height%", Integer.toString(height))).parse(0);
+				textY = (int) new Expression(ConfigValues.TEXT_Y.replace("%width%", Integer.toString(width)).replace("%height%", Integer.toString(height))).parse(0);
+			} else {
 				textX = width / 2;
 				textY = height / 4 + 48 + 72 + 12 + (modMenu?32:22);
 			}
-		}).start();
+		} catch (Throwable t) {
+			t.printStackTrace();
+			textX = width / 2;
+			textY = height / 4 + 48 + 72 + 12 + (modMenu?32:22);
+		}
 		addDrawableChild(new GuiButtonWithImage(width / 2 + 104, height / 4 + 48 + 72 + (modMenu?IASMMPos.buttonOffset():-12), btn -> {
 			if (Config.getInstance() == null) {
 				Config.load();
