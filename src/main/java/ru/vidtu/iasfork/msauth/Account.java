79fa4445fd816143e1a2f697abd02013e5870f9f
VidTu <60929459+VidTu@users.noreply.github.com>
2021-11-02 04:44:31 +0300

    Delete unused classes. (Caused errors)

--- src/main/java/ru/vidtu/iasfork/msauth/Account.java
+++
@@ -1,6 +0,0 @@
-package ru.vidtu.iasfork.msauth;
-
-public interface Account {
-	public String alias();
-	public Throwable login();
-}
--- src/main/java/ru/vidtu/iasfork/msauth/MSAuthHandler.java
+++
@@ -1,8 +0,0 @@
-package ru.vidtu.iasfork.msauth;
-
-public interface MSAuthHandler {
-	public void setState(String s);
-	public void error(Throwable t);
-	public void success(String name, String uuid, String token, String refresh);
-	public void cancellble(boolean b);
-}
