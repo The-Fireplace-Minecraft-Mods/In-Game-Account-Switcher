31cf7f65f463f766534f8e641bf21a38d43ef223
Karen/あけみ <karen@akemi.ai>
2022-08-25 05:16:19 +0900

    Backport IAS 8.0.0 to 1.19 for Quilt/Fabric, Forge >= 41.0.94 (most people will be using 41.1.0), and Forge <= 41.0.63. Fixes issue #79.
    Additional notes and changes:
    ・IAS for 1.19.2/1 (Quilt/Fabric/Forge) has had its `archivesBaseName` modified to better signify what versions are supported.
    ・IAS for 1.19.2/1 (Quilt/Fabric) explicitly conflicts against Minecraft 1.19 in order to prevent users from accidentally installing the incorrect version.
    ・IAS for 1.19 (Forge) explicitly depends on each respective version of the Forge API in order to prevent users from accidentally installing the incorrect version.
    
    Things to consider:
    ・The `*-1.19` subprojects would benefit from being renamed to `*-1.19.x` or something along those lines in order to make the project structure easier to understand for other contributors. It would also allow the `*-1.19-backport` projects to drop the `-backport` suffix entirely.

---
+++ fabric-1.19-backport/src/main/java/ru/vidtu/ias/mixins/MinecraftAccessor.java
@@ -0,0 +1,38 @@
+package ru.vidtu.ias.mixins;
+
+import com.mojang.authlib.minecraft.UserApiService;
+import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.User;
+import net.minecraft.client.gui.screens.social.PlayerSocialManager;
+import net.minecraft.client.main.GameConfig;
+import net.minecraft.client.multiplayer.ProfileKeyPairManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(Minecraft.class)
+public interface MinecraftAccessor {
+    @Accessor("user")
+    @Mutable
+    void ias$user(User s);
+
+    @Accessor("playerSocialManager")
+    @Mutable
+    void ias$playerSocialManager(PlayerSocialManager m);
+
+    @Accessor("profileKeyPairManager")
+    @Mutable
+    void ias$profileKeyPairManager(ProfileKeyPairManager m);
+
+    @Accessor("userApiService")
+    @Mutable
+    void ias$userApiService(UserApiService s);
+
+    @Invoker("createUserApiService")
+    UserApiService ias$createUserApiService(YggdrasilAuthenticationService ygg, GameConfig cfg);
+
+    @Accessor("authenticationService")
+    YggdrasilAuthenticationService ias$authenticationService();
+}
