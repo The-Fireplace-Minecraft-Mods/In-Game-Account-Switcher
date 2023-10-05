5a3f020405dd7912cea2e070a9f098bd0ec2a9b3
VidTu <imvidtu@proton.me>
2023-10-05 21:44:58 +0300

    chore: Migrate to SLF4J

--- shared/src/main/java/ru/vidtu/ias/SharedIAS.java
+++ shared/src/main/java/ru/vidtu/ias/SharedIAS.java
@@ -1,15 +1,15 @@
 package ru.vidtu.ias;
 
 import com.google.gson.Gson;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class SharedIAS {
     public static final Executor EXECUTOR = Executors.newSingleThreadExecutor(r -> new Thread(r, "IAS Executor"));
-    public static final Logger LOG = LogManager.getLogger("IAS");
+    public static final Logger LOG = LoggerFactory.getLogger("IAS");
     public static final Gson GSON = new Gson();
     public static final String[] LOADING = new String[]{"▃ ▄ ▅ ▆ ▇ █ ▇ ▆ ▅ ▄ ▃", "_ ▃ ▄ ▅ ▆ ▇ █ ▇ ▆ ▅ ▄",
             "_ _ ▃ ▄ ▅ ▆ ▇ █ ▇ ▆ ▅", "_ _ _ ▃ ▄ ▅ ▆ ▇ █ ▇ ▆", "_ _ _ _ ▃ ▄ ▅ ▆ ▇ █ ▇", "_ _ _ _ _ ▃ ▄ ▅ ▆ ▇ █",
