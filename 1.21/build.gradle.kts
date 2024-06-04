609109a3f46740e5899ad89030518f60533149ec
VidTu <imvidtu@proton.me>
2024-06-04 17:05:27 +0300

    feat: 1.21-pre2 (Fabric/Quilt)

---
+++ 1.21/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("dev.architectury.loom") version "1.6-SNAPSHOT"
+}
+
+java.sourceCompatibility = JavaVersion.VERSION_21
+java.targetCompatibility = JavaVersion.VERSION_21
+java.toolchain.languageVersion = JavaLanguageVersion.of(21)
+group = "ru.vidtu.ias"
+base.archivesName = "IAS-1.21"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.spongepowered.org/repository/maven-public/")
+}
+
+loom {
+    silentMojangMappingsLicense()
+}
+
+dependencies {
+    // Annotations
+    compileOnlyApi(libs.jetbrains.annotations)
+    compileOnlyApi(libs.error.prone.annotations)
+
+    // Minecraft
+    minecraft("com.mojang:minecraft:1.21-pre2")
+    mappings(loom.officialMojangMappings())
+
+    // Mixin
+    compileOnly(libs.mixin)
+
+    // Root
+    compileOnlyApi(rootProject)
+}
+
+tasks.withType<JavaCompile> {
+    source(rootProject.sourceSets.main.get().java)
+    options.encoding = "UTF-8"
+    options.release = 21
+}
+
+tasks.withType<ProcessResources> {
+    from(rootProject.sourceSets.main.get().resources)
+}
+
+tasks.withType<Jar> {
+    from(rootDir.resolve("LICENSE"))
+    from(rootDir.resolve("GPL"))
+    from(rootDir.resolve("NOTICE"))
+    manifest {
+        attributes(
+            "Specification-Title" to "In-Game Account Switcher",
+            "Specification-Version" to project.version,
+            "Specification-Vendor" to "VidTu",
+            "Implementation-Title" to "IAS-1.21",
+            "Implementation-Version" to project.version,
+            "Implementation-Vendor" to "VidTu"
+        )
+    }
+}
