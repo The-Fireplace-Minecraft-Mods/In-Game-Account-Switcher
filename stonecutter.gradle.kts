7184a6d576ecb66195781f69ce33381616592435
VidTu <imvidtu@proton.me>
2025-06-30 12:51:39 +0300

    feat: 1.21.7-rc2

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -21,7 +21,7 @@ plugins {
     id("dev.kikugie.stonecutter")
 }
 
-stonecutter active "1.21.6-fabric" /* [SC] DO NOT EDIT */
+stonecutter active "1.21.7-fabric" /* [SC] DO NOT EDIT */
 
 stonecutter registerChiseled tasks.register("chiseledBuild", stonecutter.chiseled) {
     group = "project"
