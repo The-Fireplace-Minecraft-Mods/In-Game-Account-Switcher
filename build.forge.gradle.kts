c55ae66c2cfd8e17868b006058ae1a94b4229e96
VidTu <imvidtu@proton.me>
2026-05-01 09:43:45 +0300

    fix: Fix compile for 1.18.2
    Now ./compileall legacy works.
    Haven't tested at runtime yet.

--- build.forge.gradle.kts
+++ build.forge.gradle.kts
@@ -139,6 +139,13 @@ dependencies {
     compileOnly(libs.jetbrains.annotations)
     compileOnly(libs.error.prone.annotations)
 
+    // TODO(VidTu): I don't know why it can't find GLFW on the classpath
+    //  with Forge 1.18.2 on the classpath. Maybe it's a Gradle cache issue.
+    //  This is quick and dirty fix for this abomination.
+    if (mcp eq "1.18.2") {
+        compileOnly("org.lwjgl:lwjgl-glfw:3.2.1")
+    }
+
     // Mixin.
     if (mcp < "1.20.6") {
         annotationProcessor("${libs.mixin.get()}:processor")
