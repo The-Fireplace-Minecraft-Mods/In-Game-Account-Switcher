2174587f64c07386335f66a728fa5c997e4d1539
VidTu <imvidtu@proton.me>
2026-08-08 23:44:01 +0300

    build: Update loaders and libs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -41,7 +41,7 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("dev.kikugie.stonecutter") version "0.9.6"
+    id("dev.kikugie.stonecutter") version "0.9.7"
 }
 
 // Project.
